4cb1aa7aaa9cf068a15faca31710d8b891642eb9
VioletQQY <forviolet771@gmail.com>
2018-05-25 17:42:43 +0800

    add new

---
+++ ccj-01-clean-varibles/scope.js
@@ -0,0 +1,21 @@
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log(i); // 10
+
+// equal
+// var i;
+// for (i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+
+var test = true;
+
+if (test) {
+  let a = 18;
+}
+
+console.log(a);
